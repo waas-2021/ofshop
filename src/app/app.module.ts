ff42ccd244e46e5e36db47cf7b5ef64cf7f795f8
gaspar_cardos <evandersoft@gmail.com>
2021-10-26 15:55:43 -0500

    modificacion de los componentes principales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule}  from '@angular/common/http';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { PaginaPrincipalComponent } from './componentes/pagina-principal/pagina-principal.component';
+import { PaginaNoEncontradaComponent } from './componentes/pagina-no-encontrada/pagina-no-encontrada.component';
+import { PaginaBusquedaProductosComponent } from './componentes/pagina-busqueda-productos/pagina-busqueda-productos.component';
+
+import { BusquedaProductosServicioService } from './servicios/productos/busqueda-productos-servicio.service';
+import { PaginaProductoComponent } from './componentes/pagina-producto/pagina-producto.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaginaPrincipalComponent,
+    PaginaNoEncontradaComponent,
+    PaginaBusquedaProductosComponent,
+    PaginaProductoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [BusquedaProductosServicioService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
