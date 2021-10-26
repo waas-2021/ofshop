1d62cb537a03e4eb2a815b0b3a8df56a94778708
gaspar_cardos <evandersoft@gmail.com>
2021-10-26 15:55:20 -0500

    ruteo inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { PaginaPrincipalComponent } from './componentes/pagina-principal/pagina-principal.component';
+import { PaginaNoEncontradaComponent } from './componentes/pagina-no-encontrada/pagina-no-encontrada.component'
+import { PaginaBusquedaProductosComponent } from './componentes/pagina-busqueda-productos/pagina-busqueda-productos.component';
+import { PaginaProductoComponent } from './componentes/pagina-producto/pagina-producto.component';
+
+const routes: Routes = [
+  { path: '', component: PaginaPrincipalComponent },
+  { path: 'busqueda', component: PaginaBusquedaProductosComponent },
+  { path: 'producto/:id', component: PaginaProductoComponent },
+  { path: '**', component: PaginaNoEncontradaComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
